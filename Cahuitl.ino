37aad6e259a25ac86c5564fd4144453165521946
Erick Vega <armsystechno@gmail.com>
2023-09-21 12:10:57 -0600

    Update and rename mqtt__DHT11_PantallaOled_ESP32.ino to Cahuitl.ino
    Cambio de nombre puesto que se reutiliza código de Cahuitl

--- Cahuitl.ino
+++ Cahuitl.ino
@@ -10,7 +10,6 @@
 -Los datos se muestran en la pantalla física cuando hay alguien cerca para verlo, y al no detectarse presencia humana por un tiempo considerable   -
 -se apaga la pantalla para ahorrar energía y mejorar la vida de la pantalla OLED.                                                                  -
 -Se publica en un broker de HiveQ los datos sensados en formato JSON y se controla el funcionamiento del ventilador con una app (IoT MQTT Panel)   -
--Basado en proyecto Cahuitl                                                                                                                        -
 ----------------------------------------------------------------------------------------------------------------------------------------------------
 */
 
